673a6308f7a1b9474c4b406dbb091b410b383ea0
Rufin Hounkpe <rhounkpe@gmail.com>
2020-09-18 19:22:22 +0200

    Event model

---
+++ src/main/java/be/digitcom/jugtours/model/Event.java
@@ -0,0 +1,16 @@
+package be.digitcom.jugtours.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Event {
+}
